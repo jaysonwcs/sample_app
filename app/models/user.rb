f119d586c823646b3a6dad0b771a46e069ee22ab
Jayson <jaysonwcs@gmail.com>
2014-04-22 20:06:00 -0300

    - Chapter 8 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  
+  #Callbacks
   before_save { email.downcase! }
+  before_create :create_remember_token
+  
+  #Validations
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
@@ -7,4 +12,20 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+  #Class methods
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  #Private methods
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
